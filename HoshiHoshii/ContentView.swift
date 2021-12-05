8e8de5c3804deab994dd8ab926df568ccb795bed
bannzai <kingkong999yhirose@gmail.com>
2021-12-05 20:55:39 +0900

    :tada: initial commit

---
+++ HoshiHoshii/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HoshiHoshii
+//
+//  Created by 廣瀬雄大 on 2021/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HoshiHoshii/HoshiHoshiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoshiHoshiiApp.swift
+//  HoshiHoshii
+//
+//  Created by 廣瀬雄大 on 2021/12/05.
+//
+
+import SwiftUI
+
+@main
+struct HoshiHoshiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
