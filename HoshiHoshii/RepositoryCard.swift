324351267b9c312c5cdf3dce1ee53067cba77055
bannzai <kingkong999yhirose@gmail.com>
2021-12-12 19:50:21 +0900

    :sparkels: fix RepositoryCardFragment

--- HoshiHoshii/RepositoryCard.swift
+++ HoshiHoshii/RepositoryCard.swift
@@ -1,27 +1,45 @@
 import SwiftUI
 
 public struct RepositoryCard: View {
+    let fragment: RepositoryCardFragment
+
     public var body: some View {
         VStack(alignment: .leading, spacing: 6) {
             HStack(alignment: .center, spacing: 10) {
                 HStack(alignment: .center, spacing: 10) {
-                    Text("Gedatsu")
+                    Text(fragment.name)
                         .font(.system(size: 17))
                         .foregroundColor(Color(red: 1, green: 1, blue: 1))
-                    Image("gedatsu")
-                        .resizable()
-                        .frame(width: 44, height: 22)
+
+                    if fragment.usesCustomOpenGraphImage, let imageURL = fragment.openGraphImageUrl {
+                        AsyncImage(url: imageURL) { phase in
+                            switch phase {
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .frame(width: 44, height: 22)
+                            case _:
+                                ProgressView()
+                            }
+                        }
+                    }
                 }
                 Spacer()
                 StarButton()
             }
             .frame(maxWidth: .infinity)
-            Text("Gedatsu provide readable format about AutoLayout error console log")
-                .font(.system(size: 14))
-                .foregroundColor(Color(red: 1, green: 1, blue: 1))
+            if let description = fragment.description {
+                Text(description)
+                    .font(.system(size: 14))
+                    .foregroundColor(Color(red: 1, green: 1, blue: 1))
+            }
         }
         .padding(.all, 16)
         .frame(maxWidth: .infinity, alignment: .topLeading)
         .background(Color(red: 0.30980393290519714, green: 0.30980393290519714, blue: 0.30980393290519714))
     }
+}
+
+extension RepositoryCardFragment: Identifiable {
+
 }
