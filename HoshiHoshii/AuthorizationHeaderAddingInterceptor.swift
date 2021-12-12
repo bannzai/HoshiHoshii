8b52d23e650c8f1a16ae710504b3d41f26cdb3bc
bannzai <kingkong999yhirose@gmail.com>
2021-12-12 17:20:05 +0900

    Using access token from outside after retrieved access token

--- HoshiHoshii/AuthorizationHeaderAddingInterceptor.swift
+++ HoshiHoshii/AuthorizationHeaderAddingInterceptor.swift
@@ -3,34 +3,22 @@ import Apollo
 import FirebaseAuth
 
 public class AuthorizationHeaderAddingInterceptor: ApolloInterceptor {
+    let accessToken: String
+    init(accessToken: String) {
+        self.accessToken = accessToken
+    }
+
     public func interceptAsync<Operation: GraphQLOperation>(
         chain: RequestChain,
         request: HTTPRequest<Operation>,
         response: HTTPResponse<Operation>?,
         completion: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) {
-            guard let currentUser = Auth.auth().currentUser else {
-                return
-            }
-
-            currentUser.getIDToken(completion: { idToken, error in
-                if let error = error {
-                    if case .networkError = AuthErrorCode(rawValue: error._code) {
-                        chain.retry(request: request, completion: completion)
-                    } else {
-                        chain.handleErrorAsync(error, request: request, response: response, completion: completion)
-                    }
-                } else if let idToken = idToken {
-                    request.addHeader(name: "Authorization", value: "Bearer \(idToken)")
-
-                    chain.proceedAsync(
-                        request: request,
-                        response: response,
-                        completion: completion
-                    )
-                } else {
-                    fatalError("unexpected error and idToken are nil")
-                }
-            })
+            request.addHeader(name: "Authorization", value: "Bearer \(accessToken)")
+            chain.proceedAsync(
+                request: request,
+                response: response,
+                completion: completion
+            )
         }
 }
 
